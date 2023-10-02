70df1dcd4f836cb0137311d022e393af1a57a554
Gautham Nair <gautham.nair.2005@outlook.com>
2023-10-02 10:26:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from cx_Freeze import setup, Executable
 setup(
     name = "DonutDB Velocity Developer Preview",
     version = "24.0.0.128",
     description = "A simple and faster serverless Relational Database Management System written in Python by Gautham Nair",
-    executables = [Executable("donutdb.py", icon="donutdb.ico")] # Add the icon argument here
+    includes = ["colorama", "prettytable", "tabulate"],
+    executables = [Executable("donutdb.py", icon="donutdb.ico")]
 )
